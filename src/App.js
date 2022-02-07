ec8d17dd453cc1a003f1ddb1c9900e4f70fe482b
ginohmk <michotall95@gmail.com>
2022-02-07 12:35:40 +0100

    Structure of the calculator

--- src/App.js
+++ src/App.js
@@ -1,27 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="output">
+          <p> 0</p>
+        </div>
+        <div className="input">
+          <div className="section-button">
+            <button className="operation" type="button">
+              AC
+            </button>
+            <button className="operation" type="button">
+              +/-
+            </button>
+            <button className="operation" type="button">
+              %
+            </button>
+            <button className="operation colored" type="button">
+              +
+            </button>
+          </div>
+          <div className="section-button">
+            <button className="operation" type="button">
+              7
+            </button>
+            <button className="operation" type="button">
+              8
+            </button>
+            <button className="operation" type="button">
+              9
+            </button>
+            <button className="operation colored" type="button">
+              x
+            </button>
+          </div>
+          <div className="section-button">
+            <button className="operation" type="button">
+              4
+            </button>
+            <button className="operation" type="button">
+              5
+            </button>
+            <button className="operation" type="button">
+              6
+            </button>
+            <button className="operation colored" type="button">
+              -
+            </button>
+          </div>
+
+          <div className="section-button">
+            <button className="operation" type="button">
+              1
+            </button>
+            <button className="operation" type="button">
+              2
+            </button>
+            <button className="operation" type="button">
+              3
+            </button>
+            <button className="operation colored" type="button">
+              ÷
+            </button>
+          </div>
+
+          <div className="section-button">
+            <button className="operation zero" type="button">
+              0
+            </button>
+            <button className="operation" type="button">
+              .
+            </button>
+            <button className="operation colored" type="button">
+              =
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
